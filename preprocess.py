fcc0a9f2b8d78eef89e9278e112c5c8c25a19246
vccheng2001 <vccheng@andrew.cmu.edu>
2022-04-16 17:30:58 -0400

    fix kmeans

--- preprocess.py
+++ preprocess.py
@@ -10,14 +10,17 @@ Histogram of Oriented Gradients
 ------------------------------------
 To compute HOGs we create a histogram for each cell of an image patch
 and then normalize over the patch.
+
+Divide image into cells of mxn pixels 
+Normalization across blocks, each of which contains several cells 
 '''
 def extract_HOG(img):
 
     winSize = (28,28) # size of sliding window run across image 
-    blockSize = (16,16)
-    blockStride = (4,4)
-    cellSize = (16,16)
-    nbins = 9
+    blockSize = (16,16) # block_size is the number of cells which fit in the patch
+    blockStride = (2,2)
+    cellSize = (4,4)  # cell_size is the size of the cells of the img patch over which to calculate the histograms
+    nbins = 9 # orientation bins
     # signedGradient=True
 
     hog = cv.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins)
